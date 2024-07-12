6eaf9d20b6aba17eeceea3c284a8c2aca601ef6c
Saahil <neon@saahild.com>
2024-07-12 10:13:02 -0400

    Donate popups

---
+++ background.js
@@ -0,0 +1,6 @@
+var instance = typeof chrome == 'undefined' ? browser : chrome
+instance.runtime.onInstalled.addListener(function (object) {
+    instance.tabs.create({url: "https://saahild.com/crunchyroll.html?fi"}, function (tab) {
+        console.log("options page opened");
+    });
+});
